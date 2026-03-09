8752e1b9065c9b42af0808208f1f600fbc3609de
DanielLopezSainz <daniel_lsainz@fr.ibm.com>
2026-03-09 13:25:00 +0100

    Add alias resolution to Vite configuration fixing UI not rendered

--- Orchestrate_Demo_Documentation/Project_vG/frontend/vite.config.js
+++ Orchestrate_Demo_Documentation/Project_vG/frontend/vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      // This tells Vite to resolve Webpack-style "~" imports to node_modules
+      { find: /^~(.+)/, replacement: '$1' }
+    ]
+  },
   build: {
     // This tells Vite to put the final GUI files in the backend's public folder
-    // Adjust this path if your 'backend' folder is at a different relative level
     outDir: '../backend/public',
     emptyOutDir: true,
   },
